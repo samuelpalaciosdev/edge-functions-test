fd05622e161fd56f2f9f2548ddad8e7f9bc284ac
Samuel Palacios <samuelpl0888@gmail.com>
2023-09-03 17:08:33 -0400

    Setup project

---
+++ components/Nav.tsx
@@ -0,0 +1,7 @@
+export default function Nav() {
+  return (
+    <nav>
+      <h1 className='text-3xl tracking-wide text-purple-600'>Nav logo</h1>
+    </nav>
+  );
+}
---
+++ lib/authOptions.ts
@@ -0,0 +1,58 @@
+import { DefaultSession, NextAuthConfig } from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import GoogleProvider from 'next-auth/providers/google';
+import prisma from './db';
+
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession['user'];
+  }
+}
+
+export const authOptions: NextAuthConfig = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id as string;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      const dbUser = await prisma.user.findFirst({
+        where: {
+          email: token?.email,
+        },
+      });
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id;
+        }
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+  },
+};
